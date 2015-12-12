e69eaf99e3289e4c8eba6ce43fef2e7aa575ac01
Nick Weedon n2798271 <nick@weedon.org.au>
2015-12-12 10:34:10 +1000

    Removed unneeded packages

--- test/test-require-deps.js
+++ test/test-require-deps.js
@@ -20,7 +20,6 @@ require.config({
     jquery: "../bower_components/jquery/dist/jquery",
     requirejs: "../bower_components/requirejs/require",
     "xhr-adaptor-js": "../dist/xhr-adaptor-js",
-    "jquery-colorbox": "../bower_components/jquery-colorbox/jquery.colorbox",
     "xhrTestUtils": "util/xhrTestUtils"
   },
   packages: [
